1dc7c6d251361dcc0d372835734e85f5f7defe57
dhananjaychoudhari <76993420+dhananjaychoudhari@users.noreply.github.com>
2021-09-08 09:35:32 +0530

    Add files via upload

---
+++ src/components/Content.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+import Sidebar from './Sidebar';
+import Task from './Task';
+
+const Content = () => {
+    const [selectedTab, setSelectedTab] = useState("INBOX")
+    return (
+        <section className="content" >
+
+            <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+            <Task selectedTab={selectedTab} />
+        </section>
+    );
+
+
+};
+
+export default Content
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../images/Logo.jpg';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav>
+                <div className="logo">
+                    <img src={logo} alt="Todolist"></img>
+                </div>
+            </nav>
+
+        </header >
+    );
+
+};
+
+export default Header;
